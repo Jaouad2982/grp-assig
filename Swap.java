d83a11b27d7ee631040c706976f6d8e3932a9b09
Jaouad2982 <Jadsarouti@outlook.com>
2020-10-24 19:54:12 -0400

    Finished swapping Strings

--- Swap.java
+++ Swap.java
@@ -9,13 +9,19 @@ public class Swap {
 		
 	}
 	public static void swappingElement(int a, int b) {
+		System.out.println("Before swapping a is "+ a + " b is " + b);
 		  a = a + b; 
 		  b = a - b; 
 		  a = a - b;
-		System.out.println("a is "+ a + " b is " + b);
+		System.out.println("After swapping a is "+ a + " b is " + b);
 	}
 	public static void swappingElement(String a, String b) {
-	
+		//append the first String a to b
+		System.out.println("before swapping The string a is  " + a + " The string b is " + b);
+		a = a + b;
+		b = a.substring(0,a.length() - b.length());
+		a = a.substring(b.length());
+		System.out.println("After swapping The string a is  " + a + " The string b is " + b);
 		
 	}
 
