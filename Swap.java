bee76fdddcbceef36029149079ed70db2bb46163
Jaouad2982 <Jadsarouti@outlook.com>
2020-10-24 19:46:37 -0400

    Finished swapping numbers

--- Swap.java
+++ Swap.java
@@ -1,10 +1,23 @@
 package com.group;
-
+//Write a program to swap 2 numbers without a temporary variable? Swap  2 strings without a temporary variable?
 public class Swap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		swappingElement(3,4);
+		swappingElement("Elephant","Dog");
+		
+	}
+	public static void swappingElement(int a, int b) {
+		  a = a + b; 
+		  b = a - b; 
+		  a = a - b;
+		System.out.println("a is "+ a + " b is " + b);
+	}
+	public static void swappingElement(String a, String b) {
+	
+		
 	}
 
 }
+ 
