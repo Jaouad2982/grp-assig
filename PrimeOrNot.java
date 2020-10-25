8ff042dbff3e24ab74e6b1aabf325dd882575ace
Jaouad2982 <Jadsarouti@outlook.com>
2020-10-24 21:53:03 -0400

    prime or not done

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -1,10 +1,28 @@
 package com.group;
 
-public class PrimeOrNot {
+import java.util.Scanner;
 
+public class PrimeOrNot {
+static boolean isPrime = true;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s  = new Scanner(System.in);
+		System.out.println("Enter  a number to check");
+		int num = s.nextInt();
+		primeNum(num);
+		if(isPrime)
+			System.out.println(num + " is a prime number");
+		else 
+			System.out.println(num + " is not a prime number");
 
 	}
-
+	
+	public static void primeNum(int num) {
+		for(int i = 2 ; i < num /2 ; i++) {
+			if(num % i == 0 ) {
+				isPrime = false;
+				break;
+			}
+		}
+	}
 }
