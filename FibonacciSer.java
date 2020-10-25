0c88b8767ceb9d5d9a4f6b619e3fe382b4e81180
Jaouad2982 <Jadsarouti@outlook.com>
2020-10-24 23:48:35 -0400

    Fibonncci- done

--- FibonacciSer.java
+++ FibonacciSer.java
@@ -1,10 +1,26 @@
 package com.group;
 
+import java.util.Scanner;
+
 public class FibonacciSer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// x n = x n-1 + x n -2;
+		// x 5 = x 4 + x 3
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int num = s.nextInt();
+		System.out.println(numFinbo(num));
+	}
+	
+	public static int numFinbo(int num) {
+		
+		if(num <=1) {
+			return num;
+		}
+		return numFinbo(num -1 ) + numFinbo(num -2);
+		
 	}
 
 }
