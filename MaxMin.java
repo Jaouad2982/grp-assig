c3fd2cde45014232331b8a5475250871b0864151
Jaouad2982 <Jadsarouti@outlook.com>
2020-10-24 23:55:56 -0400

    Max done

--- MaxMin.java
+++ MaxMin.java
@@ -6,6 +6,7 @@ public class MaxMin {
 		// TODO Auto-generated method stub
 		int [] listArr = {1,2,3,34,56,0};
 		min(listArr);
+		max(listArr);
 	}
 	
 	public static void min(int[]arr) {
@@ -15,7 +16,13 @@ public class MaxMin {
 				min = arr[i];
 		}
 		System.out.println(min);
-
 	}
-
+	public static void max(int[]arr) {
+		int max = arr[0];
+		for(int i = 0 ; i < arr.length ;i++) {
+			if(arr[i] > max)
+				max = arr[i];
+		}
+		System.out.println(max);
+	}
 }
